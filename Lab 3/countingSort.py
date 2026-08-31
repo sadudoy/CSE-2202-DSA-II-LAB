874afa0f30d76938a46684b209abeaaef8f07148
sadudoy <sadudoy29@gmail.com>
2026-08-31 22:24:15 +0600

    added comments

--- Lab 3/countingSort.py
+++ Lab 3/countingSort.py
@@ -4,10 +4,10 @@ def counting_sort(arr, n):
     newArr = [0] * (n+1)
     result = []
 
-    for num in arr:
+    for num in arr:                     # Count each number
         newArr[num]+=1
     
-    for i in range(0, n + 1):
+    for i in range(0, n + 1):           # Add numbers in sorted order
         count = newArr[i]
         while count > 0:
             result.append(i)
@@ -16,4 +16,4 @@ def counting_sort(arr, n):
     return(result)
 
 arr = [5, 3, 2, 2, 5, 1, 4]
-print(counting_sort(arr, len(arr)))
+print(counting_sort(arr, len(arr)))     # Print sorted array
